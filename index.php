fda90035b45a88799056b1469e07105d93eef1c0
oueni1932722 <oueni1932722@W10-INFO-5>
2020-01-27 21:24:45 -0500

    send help please

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oueni1932722
+ * Date: 2020-01-27
+ * Time: 16:35
+ */
---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Eyavabohmb Airport</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+
+<body style="background-image: url(airplanes-background.jpg); background-size: cover">
+<header>
+    <img src="ananananananananananananananas.png" width="50px" height="50px">
+    <h3>Eyavabohmb Airport - We're the <span style="text-decoration-line: underline; font-style: italic; font-weight: bolder;">bomb</span> amongst the competition!</h3>
+    <img src="ananananananananananananananas.png" width="50px" height="50px">
+</header>
+<div id="corps">
+    <div id="formulaireConnection">
+    <h1>Connexion</h1>
+    <form method="POST" action="index.php">
+        <table>
+            <tr>
+                <td><label for="user">Username: </label></td><td><input type="text" id="user" name="user" placeholder="Username"></td>
+            </tr>
+            <tr>
+                <td><label for="password">Password: </label></td><td><input type="password" id="password" name="password" placeholder="Password"></td>
+            </tr>
+            <tr>
+                <td><input type="button" id="register" name="register" value="Register"></td><td><input type="submit" id="login" name="login" value="Login"></td>
+            </tr>
+        </table>
+    </form>
+    </div>
+</div>
+<footer>
+    <div id="footerDiv">
+    <div>Fait par: Nicolas Ouellet-Duval</div>
+    <div>Contactez-nous: oueni1932722@etu.cegepjonquiere.qc.ca</div></div>
+</footer>
+</body>
+
+</html>
